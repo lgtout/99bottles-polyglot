03e335ea36dc6a20c31ba5bfa41c868a4655cbc7
lgtout <lagos.tout@gmail.com>
2019-02-22 18:23:55 -0800

    OO approach

--- kotlin/src/main/kotlin/BeerSong.kt
+++ kotlin/src/main/kotlin/BeerSong.kt
@@ -1,53 +1,78 @@
-object BeerSong {
-    fun verses(start: Int, end: Int): String {
-        return (start downTo end).joinToString(separator = "\n") {
-            bottleVerse(it)
+class BeerSong(private val start: Int, private val end: Int) {
+
+    val string: String
+        get() = (start downTo end).joinToString(separator = "\n") { Verse(it).string }
+
+    companion object {
+        fun verses(start: Int, end: Int): String {
+            return BeerSong(start, end).string
         }
     }
 
-    private fun bottleVerse(bottleCount: Int): String {
+    class BottleCount(private val count: Int, private val zeroCount: String) {
 
-        fun bottleCount(count: Int, zeroCount: String): String {
-
-            fun bottles(count: Int): String {
-                return if (count > 1 || count == 0) "bottles"
-                else "bottle"
-            }
-            fun firstLineBottleCount(count: Int, zeroCount: String): String {
-                return if (count == 0) zeroCount
-                else count.toString()
-            }
-
-            return "${firstLineBottleCount(count, zeroCount)} ${bottles(count)}"
+        private fun bottles(): String {
+            return if (count > 1 || count == 0) "bottles"
+            else "bottle"
+        }
+        private fun firstLineBottleCount(): String {
+            return if (count == 0) zeroCount else count.toString()
         }
 
-        val noMore = "No more"
-        val lowerCaseNoMore = noMore.toLowerCase()
+        val string: String
+            get() = "${firstLineBottleCount()} ${bottles()}"
 
-        fun firstLine(count: Int): String {
-            return "${bottleCount(count, noMore)} of beer on the wall, " +
-                    "${bottleCount(count, lowerCaseNoMore)} of beer."
-        }
-
-        fun secondLine(bottleCount: Int, remainder: Int): String {
-
-            fun one(remainder: Int): String {
-                return if (remainder == 0) "it" else "one"
-            }
-
-            fun secondLineAction(bottleCount: Int, remainder:Int): String {
-                return if (bottleCount == 0) "Go to the store and buy some more"
-                else "Take ${one(remainder)} down and pass it around"
-            }
-
-            fun secondLineResult(bottleCount: Int): String {
-                return "${bottleCount(bottleCount, lowerCaseNoMore)} of beer on the wall."
-            }
-
-            return "${secondLineAction(bottleCount, remainder)}, ${secondLineResult(remainder)}"
-        }
-
-        val remainder = if (bottleCount == 0) 99 else bottleCount - 1
-        return "${firstLine(bottleCount)}\n${secondLine(bottleCount, remainder)}\n"
     }
+
+    class FirstLine(
+        private val firstBottleCount: BottleCount,
+        private val secondBottleCount: BottleCount) {
+
+        val string: String
+            get() = "${firstBottleCount.string} of beer on the wall, " +
+                    "${secondBottleCount.string} of beer."
+
+    }
+
+    class SecondLine(
+        private val count: Int, private val remainder: Int,
+        private val remainderBottleCount: BottleCount) {
+
+        private val one: String
+            get() = if (remainder == 0) "it" else "one"
+
+        private val secondLineAction: String
+            get() = if (count == 0) "Go to the store and buy some more"
+            else "Take $one down and pass it around"
+
+        private val secondLineResult: String
+            get() = "${remainderBottleCount.string} of beer on the wall."
+
+        val string: String
+            get() = "$secondLineAction, $secondLineResult"
+
+    }
+
+    class Verse(private val bottleCount: Int) {
+
+        private val noMoreBottleCount: BottleCount
+        private val lowerCaseNoMoreBottleCount: BottleCount
+        private val remainderBottleCount: BottleCount
+        private val remainder: Int = if (bottleCount == 0) 99 else bottleCount - 1
+
+        init {
+            val noMore = "No more"
+            val lowerCaseNoMore = noMore.toLowerCase()
+
+            noMoreBottleCount = BottleCount(bottleCount, noMore)
+            lowerCaseNoMoreBottleCount = BottleCount(bottleCount, lowerCaseNoMore)
+            remainderBottleCount = BottleCount(remainder, lowerCaseNoMore)
+        }
+
+        val string: String
+            get() = "${FirstLine(noMoreBottleCount, lowerCaseNoMoreBottleCount).string}\n" +
+                    "${SecondLine(bottleCount, remainder, remainderBottleCount).string}\n"
+
+    }
+
 }
